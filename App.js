db51f806a7d7606d061cb759eefcc60621264ea6
yuri <yuri.abaev@zooz.com>
2019-10-27 09:35:25 +0200

    added better reading from BT

--- App.js
+++ App.js
@@ -7,12 +7,12 @@
  */
 //import EasyBluetooth from 'easy-bluetooth-classic'
 import BluetoothSerial from 'react-native-bluetooth-serial'
-import Toast from '@remobile/react-native-toast'
 import React, { Component } from 'react'
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  TextInput,
   View,
   Button,
   Text,
@@ -25,28 +25,18 @@ import {
 } from 'react-native/Libraries/NewAppScreen'
 
 const log = (...args) => {
-  Toast.showShortBottom(...args)
   console.log(...args)
 }
 
 export default class App extends Component {
+  state = {
+    connected: false,
+    watering_duration: 0,
+    desiredDuration: 0
+  }
+
   componentDidMount () {
-    Promise.all([
-      BluetoothSerial.isEnabled(),
-      BluetoothSerial.list()
-    ])
-      .then((values) => {
-        const [isEnabled, devices] = values
-        log('isEnabled,  devices', isEnabled, devices)
-        this.setState({isEnabled, devices})
-        const device = devices[0]
-        BluetoothSerial.connect(device.id)
-          .then((res) => {
-            log(`Connected to device  ${device.name}`)
-            this.setState({device, connected: true, connecting: false})
-          })
-          .catch((err) => log(err.message))
-      })
+    this.connect()
 
     BluetoothSerial.on('error', (err) => log(`Error: ${err.message}`))
     BluetoothSerial.on('data', (data) => log('data:' + data))
@@ -59,6 +49,7 @@ export default class App extends Component {
     })
     BluetoothSerial.on('read', (data) => {
       console.log('Reading data: ', data)
+      this.setState({response: data})
     })
   }
 
@@ -81,24 +72,79 @@ export default class App extends Component {
       })
   }
 
-  doWater = () => {
-    return BluetoothSerial.write('water')
-      .then((res) => {
-        log('Successfuly wrote to device ' + res)
-        this.setState({connected: true})
-      })
-      .catch((err) => log(err.message))
+  doWater = async () => {
+    try {
+      await this.sendCommand('water')
+
+    } catch
+      (err) {
+      log(err.message)
+    }
   }
 
-  getDurationTimeout = () => {
-    return BluetoothSerial.write('get_watering_duration')
-      .then((res) => {
-        log('get_watering_duration:' + res)
-        BluetoothSerial.readFromDevice().then((res) => {
-          log('res:' + res)
-        })
+  getDurationTimeout = async () => {
+    try {
+      const watering_duration = await this.sendCommand('get_watering_duration')
+
+      this.setState({watering_duration, desiredDuration: watering_duration})
+    } catch
+      (err) {
+      log(err.message)
+    }
+  }
+
+  setDurationTimeout = async () => {
+    try {
+      await this.sendCommand(`set_watering_duration ${this.state.desiredDuration}`)
+
+    } catch
+      (err) {
+      log(err.message)
+    }
+  }
+
+  waitForAnswer = async () => {
+    return new Promise((resolve, reject) => {
+      this.setState({response: {data: ''}}, () => {
+
+
+        let times = 0
+        const nIntervId = setInterval(() => {
+          console.log('waiting', this.state.response)
+          times++
+          if (this.state.response.data !== '' || times > 3) {
+            clearInterval(nIntervId)
+            const response = this.state.response.data.replace(/\n|\r/g, '')
+            console.log('responding', response)
+            resolve(response)
+          }
+        }, 1000)
+
       })
-      .catch((err) => log(err.message))
+    })
+
+  }
+
+  sendCommand = async (command) => {
+    try {
+      log('sending:' + command)
+      await BluetoothSerial.write(command)
+      // const responseWithLineBreaks = await BluetoothSerial.readFromDevice()
+      // log('responseWithLineBreaks: '+responseWithLineBreaks)
+
+      await BluetoothSerial.withDelimiter('\r\n')
+
+      const response = await this.waitForAnswer() //'ff'//responseWithLineBreaks.replace(/\n|\r/g, '')
+      log('res:' + response)
+      return response
+    } catch
+      (err) {
+      log(err.message)
+    }
+  }
+
+  onChangeDurationText = (text) => {
+    this.setState({desiredDuration: text})
   }
 
   render () {
@@ -115,28 +161,30 @@ export default class App extends Component {
                 <Text style={styles.footer}>Engine: Hermes</Text>
               </View>
             )}
+            <Text style={styles.sectionTitle}>connected: {this.state.connected.toString()}</Text>
             <Button title={'connect'} onPress={this.connect}/>
             <Text style={styles.sectionTitle}>Step One</Text>
             <Button title={'water'} onPress={this.doWater}/>
             <Text style={styles.sectionTitle}>Step One</Text>
             <Button title={'Get Duration Timeout'} onPress={this.getDurationTimeout}/>
+            <TextInput
+              style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+              onChangeText={text => this.onChangeDurationText(text)}
+              value={this.state.desiredDuration.toString()}
+              placeHolder="fff"
+            />
+            <Button title={'Set Duration Timeout'} onPress={this.setDurationTimeout}/>
+
+
             <View style={styles.body}>
-              <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Step One</Text>
-                <Text style={styles.sectionDescription}>
-                  Edit <Text style={styles.highlight}>App.j9cs</Text> to change this
-                  screen and then come back to see your edits.
-                </Text>
-              </View>
-
-
+              <Text style={styles.sectionTitle}>{JSON.stringify(this.state, null, 2)}</Text>
             </View>
           </ScrollView>
         </SafeAreaView>
       </>
     )
   }
-};
+}
 
 const styles = StyleSheet.create({
   scrollView: {
